e0743924e737074183adeb43e1d469a560483dc3
sAyflort <neefe.saratan.life.life@gmail.com>
2022-03-06 15:34:57 +0300

    Initial commit

---
+++ core/src/com/star/app/game/GameController.java
@@ -0,0 +1,104 @@
+package com.star.app.game;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.star.app.screen.ScreenManager;
+
+public class GameController {
+    private Background background;
+    private BulletController bulletController;
+    private AsteroidController asteroidController;
+    private ParticleController particleController;
+    private Hero hero;
+    private Vector2 tempVec;
+
+    public ParticleController getParticleController() {
+        return particleController;
+    }
+
+    public AsteroidController getAsteroidController() {
+        return asteroidController;
+    }
+
+    public BulletController getBulletController() {
+        return bulletController;
+    }
+
+    public Background getBackground() {
+        return background;
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public GameController() {
+        this.background = new Background(this);
+        this.bulletController = new BulletController(this);
+        this.asteroidController = new AsteroidController(this);
+        this.particleController = new ParticleController();
+        this.hero = new Hero(this);
+        this.tempVec = new Vector2();
+
+        for (int i = 0; i < 3; i++) {
+            asteroidController.setup(MathUtils.random(0, ScreenManager.SCREEN_WIDTH),
+                    MathUtils.random(0, ScreenManager.SCREEN_HEIGHT),
+                    MathUtils.random(-150, 150), MathUtils.random(-150, 150), 1.0f);
+        }
+    }
+
+    public void update(float dt) {
+        background.update(dt);
+        bulletController.update(dt);
+        asteroidController.update(dt);
+        particleController.update(dt);
+        hero.update(dt);
+        checkCollisions();
+    }
+
+
+    public void checkCollisions() {
+        //столкновение астероидов и героя
+        for (int i = 0; i < asteroidController.getActiveList().size(); i++) {
+            Asteroid a = asteroidController.getActiveList().get(i);
+            if (hero.getHitArea().overlaps(a.getHitArea())) {
+                float dst = a.getPosition().dst(hero.getPosition());
+                float halfOverLen = (a.getHitArea().radius + hero.getHitArea().radius - dst) / 2.0f;
+                tempVec.set(hero.getPosition()).sub(a.getPosition()).nor();
+                hero.getPosition().mulAdd(tempVec, halfOverLen);
+                a.getPosition().mulAdd(tempVec, -halfOverLen);
+
+                float sumScl = hero.getHitArea().radius * 2 + a.getHitArea().radius;
+                hero.getVelocity().mulAdd(tempVec, 200.0f * a.getHitArea().radius / sumScl);
+                a.getVelocity().mulAdd(tempVec, -200.0f * hero.getHitArea().radius / sumScl);
+
+                if (a.takeDamage(2)) {
+                    hero.addScore(a.getHpMax() * 50);
+                }
+                hero.takeDamage(2);
+            }
+        }
+
+        //столкновение пуль и астероидов
+        for (int i = 0; i < bulletController.getActiveList().size(); i++) {
+            Bullet b = bulletController.getActiveList().get(i);
+            for (int j = 0; j < asteroidController.getActiveList().size(); j++) {
+                Asteroid a = asteroidController.getActiveList().get(j);
+                if (a.getHitArea().contains(b.getPosition())) {
+                    particleController.setup(b.getPosition().x + MathUtils.random(-4, 4), b.getPosition().y + MathUtils.random(-4, 4),
+                            b.getVelocity().x * -0.3f + MathUtils.random(-30, 30), b.getVelocity().y * -0.3f + MathUtils.random(-30, 30),
+                            0.2f,
+                            2.5f, 1.2f,
+                            1.0f, 1.0f, 1.0f, 1.0f,
+                            0.0f, 0.1f, 1.0f, 0.0f);
+
+                    b.deactivate();
+                    if (a.takeDamage(1)) {
+                        hero.addScore(a.getHpMax() * 100);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ core/src/com/star/app/game/Hero.java
@@ -0,0 +1,196 @@
+package com.star.app.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.star.app.screen.ScreenManager;
+import com.star.app.screen.utils.Assets;
+
+public class Hero {
+    private GameController gc;
+    private TextureRegion texture;
+    private Vector2 position;
+    private Vector2 velocity;
+    private float angle;
+    private float enginePower;
+    private float fireTimer;
+    private int score;
+    private int scoreView;
+    private int hp;
+    private int hpMax;
+    private StringBuilder sb;
+    private Circle hitArea;
+    private Weapon currentWeapon;
+
+    public Circle getHitArea() {
+        return hitArea;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Vector2 getVelocity() {
+        return velocity;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public Hero(GameController gc) {
+        this.gc = gc;
+        this.texture = Assets.getInstance().getAtlas().findRegion("ship");
+        this.position = new Vector2(640, 360);
+        this.velocity = new Vector2(0, 0);
+        this.angle = 0.0f;
+        this.enginePower = 700.0f;
+        this.hpMax = 100;
+        this.hp = hpMax;
+        this.sb = new StringBuilder();
+        this.hitArea = new Circle(position, 28);
+
+        this.currentWeapon = new Weapon(gc, this,0.2f,1, 700, 100,
+                new Vector3[]{
+                        new Vector3(28, 0,0),
+                        new Vector3(28, -90,-10),
+                        new Vector3(28, 90,10),
+                });
+    }
+
+    public void addScore(int amount) {
+        score += amount;
+    }
+
+    public void takeDamage(int amount) {
+        hp -= amount;
+    }
+
+    public void renderGUI(SpriteBatch batch, BitmapFont font) {
+        sb.setLength(0);
+        sb.append("SCORE: ").append(scoreView).append("\n");
+        sb.append("HP: ").append(hp).append("/").append(hpMax).append("\n");
+        sb.append("BULLETS: ").append(currentWeapon.getCurBullets()).append("/").append(currentWeapon.getMaxBullets()).append("\n");
+        font.draw(batch, sb, 20, 700);
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, position.x - 32, position.y - 32, 32, 32,
+                64, 64, 1, 1, angle);
+    }
+
+    public void update(float dt) {
+        fireTimer += dt;
+        updateScore(dt);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            tryToFire();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            angle += 180 * dt;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            angle -= 180 * dt;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            velocity.x += MathUtils.cosDeg(angle) * enginePower * dt;
+            velocity.y += MathUtils.sinDeg(angle) * enginePower * dt;
+
+            float bx = position.x + MathUtils.cosDeg(angle + 180) * 25;
+            float by = position.y + MathUtils.sinDeg(angle + 180) * 25;
+
+            for (int i = 0; i < 3; i++) {
+                gc.getParticleController().setup(bx + MathUtils.random(-4, 4), by + MathUtils.random(-4, 4),
+                        velocity.x * -0.1f + MathUtils.random(-20, 20), velocity.y * -0.1f + MathUtils.random(-20, 20),
+                        0.5f,
+                        1.2f, 0.2f,
+                        1.0f, 0.5f, 0.0f, 1.0f,
+                        1.0f, 1.0f, 1.0f, 0.0f);
+            }
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            velocity.x += MathUtils.cosDeg(angle) * enginePower * -0.5f * dt;
+            velocity.y += MathUtils.sinDeg(angle) * enginePower * -0.5f * dt;
+
+            float bx = position.x + MathUtils.cosDeg(angle - 90) * 25;
+            float by = position.y + MathUtils.sinDeg(angle - 90) * 25;
+
+            for (int i = 0; i < 3; i++) {
+                gc.getParticleController().setup(bx + MathUtils.random(-4, 4), by + MathUtils.random(-4, 4),
+                        velocity.x * 0.1f + MathUtils.random(-20, 20), velocity.y * 0.1f + MathUtils.random(-20, 20),
+                        0.2f,
+                        1.2f, 0.2f,
+                        1.0f, 0.5f, 0.0f, 1.0f,
+                        1.0f, 1.0f, 1.0f, 0.0f);
+            }
+
+            bx = position.x + MathUtils.cosDeg(angle + 90) * 25;
+            by = position.y + MathUtils.sinDeg(angle + 90) * 25;
+
+            for (int i = 0; i < 3; i++) {
+                gc.getParticleController().setup(bx + MathUtils.random(-4, 4), by + MathUtils.random(-4, 4),
+                        velocity.x * 0.1f + MathUtils.random(-20, 20), velocity.y * 0.1f + MathUtils.random(-20, 20),
+                        0.2f,
+                        1.2f, 0.2f,
+                        1.0f, 0.5f, 0.0f, 1.0f,
+                        1.0f, 1.0f, 1.0f, 0.0f);
+            }
+
+        }
+        position.mulAdd(velocity, dt);
+        hitArea.setPosition(position);
+        float stopKoef = 1.0f - dt;
+        if (stopKoef < 0.0f) {
+            stopKoef = 0.0f;
+        }
+        velocity.scl(stopKoef);
+
+        checkBorders();
+    }
+
+    private void updateScore(float dt) {
+        if (scoreView < score) {
+            scoreView += 1500 * dt;
+            if (scoreView > score) {
+                scoreView = score;
+            }
+        }
+    }
+
+    private void tryToFire() {
+        if (fireTimer > 0.2) {
+            fireTimer = 0.0f;
+            currentWeapon.fire();
+        }
+    }
+
+    public void checkBorders() {
+        if (position.x < 32) {
+            position.x = 32f;
+            velocity.x *= -0.5f;
+        }
+        if (position.x > ScreenManager.SCREEN_WIDTH - 32) {
+            position.x = ScreenManager.SCREEN_WIDTH - 32f;
+            velocity.x *= -0.5f;
+        }
+        if (position.y < 32) {
+            position.y = 32f;
+            velocity.y *= -0.5f;
+        }
+        if (position.y > ScreenManager.SCREEN_HEIGHT - 32) {
+            position.y = ScreenManager.SCREEN_HEIGHT - 32f;
+            velocity.y *= -0.5f;
+        }
+    }
+}
